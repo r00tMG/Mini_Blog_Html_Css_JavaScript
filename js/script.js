63541328688bd7d6d5153a1f75c941c33056d48b
r00tMG <meissagningue7@gmail.com>
2024-04-12 18:11:27 +0000

    ajout système de commentaire

--- js/script.js
+++ js/script.js
@@ -1,54 +1,21 @@
-// Initialization for ES Users
-// import { Input, Ripple, initMDB } from "mdb-ui-kit";
 
-// initMDB({ Input, Ripple });
 
-// const a = document.querySelector('a')
+function handleSearch(event) {
+    event.preventDefault();
+    const searchTerm = document.getElementById("search-input").value.toLowerCase();
+    const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(searchTerm));
+    displaySearchResults(filteredArticles);
+}
+document.getElementById("search-form").addEventListener("submit", handleSearch);
 
-// console.log(
-//     a.nodeName,
-    // a.innerHTML
-    // a.innerText
-    // a.textContent
-// )
-// a.inne rHTML = "hello"
 
-// a.setAttribute("href", "https://www.baid")
-// a.removeAttribute("href")
-// console.log(a.getAttribute("class"))
-// console.log(a.classList.add("bg-danger"))
-// console.log(a.classList.remove("navbar-brand"))
 
-// setInterval(()=>{
-//     a.classList.toggle("bg-primary")
-//     a.classList.remove("bg-danger")
-// },1000)
-
-// a.style.color="red";
-// console.log(a.style.color)
-// console.log(getComputedStyle(a).color);
-
-// const newEl = document.createElement('a')
-// newEl.innerHTML = "Hello world"
-// document.querySelector('a').appendChild(newEl)
-
-// const link = document.querySelector("a")
-// console.log(
-    // link.querySelector("img")
-    // link.children
-    // link.childNodes
-    // link.parentElement
-// )
-
-// Écoute l'événement de soumission du formulaire
 document.getElementById("article-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Empêche le rechargement de la page
-    
-    // Récupère les valeurs saisies par l'utilisateur
+    event.preventDefault(); 
     const title = document.getElementById("article-title").value;
     const content = document.getElementById("article-content").value;
+
     
-    // Crée un nouvel élément d'article
     const newArticle = `
         <div class="container">
             <article class="article mb-4 bg-light rounded-4">
@@ -61,12 +28,59 @@ document.getElementById("article-form").addEventListener("submit", function(even
                     <p class="article_contenue">${content}</p>
                 </div>
             </article>
+           
         </div>
     `;
+
     
-    // Ajoute l'élément d'article à la section des articles
+    
     document.querySelector(".d_flex").innerHTML += newArticle;
     
-    // Réinitialise le formulaire
     document.getElementById("article-form").reset();
+
+    
+
+    commentForm.reset();
 });
+
+
+const commentForm = document.querySelector('.comment-form');
+
+commentForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const commentName = document.getElementById('comment-name').value;
+    const commentContent = document.getElementById('comment-content').value;
+
+    const newComment = document.createElement('li');
+    newComment.classList.add('comment');
+    newComment.innerHTML = `
+        <div class="comment-author">
+        <div class="profile-photo ">
+                    <img class="rounded rounded-circle" src="img/profile-photo.jpeg" width="50px" height="50px" alt="">
+        </div> 
+        ${commentName}
+        </div>
+        <div class="comment-content">Contenue :${commentContent}</div>
+    `;
+
+    const commentsList = commentForm.parentElement.querySelector('.comments-list');
+
+    commentsList.appendChild(newComment);
+
+    commentForm.reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
